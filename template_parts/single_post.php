052549581567968b5ac358b9dba8528044ff31cf
Spartan-Coder89 <sjiloma1389@gmail.com>
2024-11-30 06:53:04 +0800

    Finished single post page

--- template_parts/single_post.php
+++ template_parts/single_post.php
@@ -11,53 +11,7 @@
             <figure class="featured_img">
                 <img src="<?php echo THEME_URI; ?>/assets/images/sample_image.jpg" alt="">
             </figure>
-            <section>
-                <h2>Heading</h2>
-                <p>
-                    Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt. Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt.
-                </p>
-                <p>
-                    Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt.
-                </p>
-            </section>
-            <section>
-                <h2>List Items</h2>
-                <ul>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. 
-                    </li>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Sed aliquet mauris id mauris vestibulum, eu molestie lacus blandit. Nullam aliquet lobortis risus eget tincidunt. Mauris vitae nunc ac tellus mollis iaculis tempor eget tellus. Nunc et ipsum a justo commodo egestas nec nec sem.
-                    </li>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam.
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <h2>Heading</h2>
-                <p>
-                    Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt. Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt.
-                </p>
-                <p>
-                    Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Donec auctor id purus ac scelerisque. Sed ac nulla at dui luctus consequat. Maecenas dictum maximus purus sed tincidunt.
-                </p>
-            </section>
-            <img src="<?php echo THEME_URI; ?>/assets/images/sample_image.jpg" alt="">
-            <section>
-                <h2>List Items</h2>
-                <ol>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. 
-                    </li>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam. Sed aliquet mauris id mauris vestibulum, eu molestie lacus blandit. Nullam aliquet lobortis risus eget tincidunt. Mauris vitae nunc ac tellus mollis iaculis tempor eget tellus. Nunc et ipsum a justo commodo egestas nec nec sem.
-                    </li>
-                    <li>
-                        Mauris leo dolor, egestas ac ex sed, placerat pulvinar ante. In a neque blandit, scelerisque tellus at, dignissim leo. In nec bibendum urna. Donec sed lacinia diam.
-                    </li>
-                </ol>
-            </section>
+            <?php echo get_the_content($post->ID); ?>
         </div>
     </div>
 </main>
